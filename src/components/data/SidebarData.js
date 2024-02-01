a56bff61b6937425df26c094b9ffb10847353463
Theodoraldo <theodoraldo@gmail.com>
2024-02-01 21:25:51 +0000

    Add sidebar links data

---
+++ src/components/data/SidebarData.js
@@ -0,0 +1,14 @@
+import * as FaIcons from "react-icons/fa";
+
+export const SidebarData = [
+  {
+    title: "Genre of Books",
+    path: "/genre",
+    icon: "",
+  },
+  {
+    title: "Patrons",
+    path: "/patrons",
+    icon: "",
+  },
+];
