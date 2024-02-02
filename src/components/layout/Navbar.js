8005fe35f93f644c8ce249c289275dbdc12f141f
Theodoraldo <theodoraldo@gmail.com>
2024-02-02 07:16:53 +0000

    Changed classes to className

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,13 +5,13 @@ import user from "../../assets/user.svg";
 export default function Navbar() {
   return (
     <React.Fragment>
-      <nav class="w-full h-25 bg-gray-800">
-        <div class="px-3 py-3 lg:px-5 lg:pl-3">
-          <div class="flex items-center justify-between">
-            <div class="flex items-center justify-start rtl:justify-end">
-              <div class="flex ms-2 md:me-24 gap-5">
+      <nav className="w-full h-25 bg-gray-800">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start rtl:justify-end">
+              <div className="flex ms-2 md:me-24 gap-5">
                 <img src={book} alt="library logo" width={50} />
-                <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
+                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
                   BookShelf Library
                 </span>
               </div>
