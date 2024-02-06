395491b186b316a9b71caf6610e5900603610777
Theodoraldo <theodoraldo@gmail.com>
2024-02-06 21:08:57 +0000

    Modify files

--- src/components/pages/Admin/Genre/CreateGenre.js
+++ src/components/pages/Admin/Genre/CreateGenre.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const CreateGenre = () => {
   const initialValues = {
@@ -22,16 +22,14 @@ const CreateGenre = () => {
     return errors;
   };
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validate,
-  });
-
   return (
-    <>
-      <div className="text-2xl font-bold">Create New Genre</div>
-      <form onSubmit={formik.handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="text-2xl font-bold">Create New Genre</div>
         <div className="mb-4 mt-4">
           <label
             className="block text-gray-700 text-sm mb-2"
@@ -39,19 +37,18 @@ const CreateGenre = () => {
           >
             Genre Name:
           </label>
-          <input
+          <Field
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline"
             id="genreName"
+            name="genreName"
             type="text"
             placeholder="Genre Name"
-            onChange={formik.handleChange}
-            value={formik.values.genreName}
           />
-          {formik.errors.genreName ? (
-            <div className="text-red-500 text-xs">
-              {formik.errors.genreName}
-            </div>
-          ) : null}
+          <ErrorMessage
+            name="genreName"
+            component="div"
+            className="text-red-500 text-xs"
+          />
         </div>
         <div className="mb-4 mt-4">
           <label
@@ -60,19 +57,18 @@ const CreateGenre = () => {
           >
             Description:
           </label>
-          <textarea
+          <Field
             className="appearance-none border rounded w-full h-40 py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline"
             id="description"
-            type="textarea"
+            as="textarea"
+            name="description"
             placeholder="Description"
-            onChange={formik.handleChange}
-            value={formik.values.description}
           />
-          {formik.errors.description ? (
-            <div className="text-red-500 text-xs">
-              {formik.errors.description}
-            </div>
-          ) : null}
+          <ErrorMessage
+            name="description"
+            component="div"
+            className="text-red-500 text-xs"
+          />
         </div>
         <div className="flex items-center justify-between">
           <button
@@ -82,8 +78,8 @@ const CreateGenre = () => {
             Save
           </button>
         </div>
-      </form>
-    </>
+      </Form>
+    </Formik>
   );
 };
 
