1098def6e20276240215f72ae2ad0f7e3785e148
Theodoraldo <theodoraldo@gmail.com>
2024-02-10 08:43:08 +0000

    Create slice to retrieve genre data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import getAllGenreSlice from "./Genre/getGenreDataSlice.js";
 import getAllBookSlice from "./Book/getBookDataSlice.js";
+import getOneGenreSlice from "./Genre/getOneGenreSlice.js";
 
 const store = configureStore({
   reducer: {
     getAllGenres: getAllGenreSlice,
     getAllBooks: getAllBookSlice,
+    getOneGenre: getOneGenreSlice,
   },
 });
 
