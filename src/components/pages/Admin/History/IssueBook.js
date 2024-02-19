650f674614f6e2ce4294cb392a9cf5fbeb46d906
Theodoraldo <theodoraldo@gmail.com>
2024-02-19 09:50:59 +0000

    Modify implementation of issue book logic

--- src/components/pages/Admin/History/IssueBook.js
+++ src/components/pages/Admin/History/IssueBook.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllBook } from "../../../../redux/Book/getBookDataSlice";
 import { fetchAllPatrons } from "../../../../redux/Patron/getPatronDataSlice";
+import { postIssueBook } from "../../../../redux/History/BorrowBookDataSlice";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useSelector, useDispatch } from "react-redux";
 import Autosuggest from "react-autosuggest";
 
 const IssueBook = () => {
   const dispatch = useDispatch();
-  const { getAllBookData } = useSelector((state) => state.getAllBooks);
-  const { getAllPatronData } = useSelector((state) => state.getAllPatrons);
+  const {
+    getAllBookData,
+    loading: bookLoading,
+    error: bookError,
+  } = useSelector((state) => state.getAllBooks);
+  const {
+    getAllPatronData,
+    loading: patronLoading,
+    error: patronError,
+  } = useSelector((state) => state.getAllPatrons);
 
   const initialValues = {
     borrowDate: "",
@@ -31,8 +40,11 @@ const IssueBook = () => {
   const [inputPatron, setInputPatron] = useState("");
   const [suggestionPatrons, setSuggestionPatrons] = useState([]);
 
-  const onSubmit = (values) => {
-    console.log("Data", values);
+  const onSubmit = (values, onSubmitProps) => {
+    dispatch(postIssueBook(values));
+    setInputBook("");
+    setInputPatron("");
+    onSubmitProps.resetForm(initialValues);
   };
 
   const validate = (values) => {
@@ -69,181 +81,203 @@ const IssueBook = () => {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validate={validate}
-      onSubmit={onSubmit}
-    >
-      {({ setFieldValue }) => (
-        <Form>
-          <div className="text-2xl font-bold">Issue A Book</div>
-          <div className="mb-4 mt-4">
-            <label
-              className="block text-gray-700 text-sm mb-2"
-              htmlFor="patronId"
-            >
-              Library Patron:
-            </label>
-            <Autosuggest
-              theme={{
-                container: {
-                  position: "relative",
-                },
-                suggestionsContainer: {
-                  position: "absolute",
-                  top: "100%",
-                  left: 0,
-                  zIndex: 10,
-                  backgroundColor: "#fff",
-                  borderRadius: "4px",
-                  width: "100%",
-                },
-                suggestionsList: {
-                  margin: 0,
-                  padding: 0,
-                  listStyleType: "none",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                },
-                suggestion: {
-                  padding: "10px",
-                  cursor: "pointer",
-                },
-                suggestionHighlighted: {
-                  backgroundColor: "#f0f0f0",
-                },
-              }}
-              inputProps={{
-                placeholder: "Search borrower",
-                className:
-                  "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline",
-                id: "patronId",
-                type: "text",
-                name: "patronId",
-                value: inputPatron,
-                onChange: (_, { newValue }) => {
-                  setInputPatron(newValue);
-                  setFieldValue("patronId", "");
-                },
-              }}
-              suggestions={suggestionPatrons}
-              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-              onSuggestionsClearRequested={onSuggestionsClearRequested}
-              onSuggestionSelected={(_, { suggestion }) => {
-                setFieldValue("patronId", suggestion.id);
-              }}
-              getSuggestionValue={(suggestion) =>
-                suggestion.firstname + " " + suggestion.lastname
-              }
-              renderSuggestion={(suggestion) => (
-                <div>
-                  {suggestion.firstname} {suggestion.lastname}
-                </div>
-              )}
-            />
-            <ErrorMessage
-              name="patronId"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
-          <div className="mb-4 mt-4">
-            <label
-              className="block text-gray-700 text-sm mb-2"
-              htmlFor="bookId"
-            >
-              Issued Book:
-            </label>
-            <Autosuggest
-              theme={{
-                container: {
-                  position: "relative",
-                },
-                suggestionsContainer: {
-                  position: "absolute",
-                  top: "100%",
-                  left: 0,
-                  zIndex: 10,
-                  backgroundColor: "#fff",
-                  borderRadius: "4px",
-                  width: "100%",
-                },
-                suggestionsList: {
-                  margin: 0,
-                  padding: 0,
-                  listStyleType: "none",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                },
-                suggestion: {
-                  padding: "10px",
-                  cursor: "pointer",
-                },
-                suggestionHighlighted: {
-                  backgroundColor: "#f0f0f0",
-                },
-              }}
-              inputProps={{
-                placeholder: "Search book",
-                className:
-                  "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline",
-                id: "bookId",
-                type: "text",
-                name: "bookId",
-                value: inputBook,
-                onChange: (_, { newValue }) => {
-                  setInputBook(newValue);
-                  setFieldValue("bookId", "");
-                },
-              }}
-              suggestions={suggestionBooks}
-              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-              onSuggestionsClearRequested={onSuggestionsClearRequested}
-              onSuggestionSelected={(_, { suggestion }) => {
-                setFieldValue("bookId", suggestion.id);
-              }}
-              getSuggestionValue={(suggestion) => suggestion.title}
-              renderSuggestion={(suggestion) => <div>{suggestion.title}</div>}
-            />
-            <ErrorMessage
-              name="bookId"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
-
-          <div className="flex justify-between gap-5 mt-2">
-            <div className="mb-2 mt-2 w-full">
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={onSubmit}
+      >
+        {({ setFieldValue }) => (
+          <Form>
+            <div className="text-2xl font-bold">Issue A Book</div>
+            <div className="mb-4 mt-4">
               <label
                 className="block text-gray-700 text-sm mb-2"
-                htmlFor="borrowDate"
+                htmlFor="patronId"
               >
-                Borrow Date:
+                Library Patron:
               </label>
-              <Field
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline"
-                id="borrowDate"
-                type="date"
-                name="borrowDate"
+              <Autosuggest
+                theme={{
+                  container: {
+                    position: "relative",
+                  },
+                  suggestionsContainer: {
+                    position: "absolute",
+                    top: "100%",
+                    left: 0,
+                    zIndex: 10,
+                    backgroundColor: "#fff",
+                    borderRadius: "4px",
+                    width: "100%",
+                  },
+                  suggestionsList: {
+                    margin: 0,
+                    padding: 0,
+                    listStyleType: "none",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                  },
+                  suggestion: {
+                    padding: "10px",
+                    cursor: "pointer",
+                  },
+                  suggestionHighlighted: {
+                    backgroundColor: "#f0f0f0",
+                  },
+                }}
+                inputProps={{
+                  placeholder: "Search borrower",
+                  className:
+                    "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline",
+                  id: "patronId",
+                  type: "text",
+                  name: "patronId",
+                  value: inputPatron,
+                  onChange: (_, { newValue }) => {
+                    setInputPatron(newValue);
+                    setFieldValue("patronId", "");
+                  },
+                }}
+                suggestions={suggestionPatrons}
+                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={onSuggestionsClearRequested}
+                onSuggestionSelected={(_, { suggestion }) => {
+                  setFieldValue("patronId", suggestion.id);
+                }}
+                getSuggestionValue={(suggestion) =>
+                  suggestion.firstname + " " + suggestion.lastname
+                }
+                renderSuggestion={(suggestion) => (
+                  <div>
+                    {suggestion.firstname} {suggestion.lastname}
+                  </div>
+                )}
               />
               <ErrorMessage
-                name="borrowDate"
+                name="patronId"
                 component="div"
-                className="text-red-500 text-sm"
+                className="text-red-500 text-xs"
               />
             </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded border border-gray-400 shadow-lg focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Issue Book
-            </button>
-          </div>
-        </Form>
+            <div className="mb-4 mt-4">
+              <label
+                className="block text-gray-700 text-sm mb-2"
+                htmlFor="bookId"
+              >
+                Issued Book:
+              </label>
+              <Autosuggest
+                theme={{
+                  container: {
+                    position: "relative",
+                  },
+                  suggestionsContainer: {
+                    position: "absolute",
+                    top: "100%",
+                    left: 0,
+                    zIndex: 10,
+                    backgroundColor: "#fff",
+                    borderRadius: "4px",
+                    width: "100%",
+                  },
+                  suggestionsList: {
+                    margin: 0,
+                    padding: 0,
+                    listStyleType: "none",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                  },
+                  suggestion: {
+                    padding: "10px",
+                    cursor: "pointer",
+                  },
+                  suggestionHighlighted: {
+                    backgroundColor: "#f0f0f0",
+                  },
+                }}
+                inputProps={{
+                  placeholder: "Search book",
+                  className:
+                    "appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline",
+                  id: "bookId",
+                  type: "text",
+                  name: "bookId",
+                  value: inputBook,
+                  onChange: (_, { newValue }) => {
+                    setInputBook(newValue);
+                    setFieldValue("bookId", "");
+                  },
+                }}
+                suggestions={suggestionBooks}
+                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={onSuggestionsClearRequested}
+                onSuggestionSelected={(_, { suggestion }) => {
+                  setFieldValue("bookId", suggestion.id);
+                }}
+                getSuggestionValue={(suggestion) => suggestion.title}
+                renderSuggestion={(suggestion) => <div>{suggestion.title}</div>}
+              />
+              <ErrorMessage
+                name="bookId"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
+
+            <div className="flex justify-between gap-5 mt-2">
+              <div className="mb-2 mt-2 w-full">
+                <label
+                  className="block text-gray-700 text-sm mb-2"
+                  htmlFor="borrowDate"
+                >
+                  Borrow Date:
+                </label>
+                <Field
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-blue-300 focus:shadow-outline"
+                  id="borrowDate"
+                  type="date"
+                  name="borrowDate"
+                />
+                <ErrorMessage
+                  name="borrowDate"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded border border-gray-400 shadow-lg focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Issue Book
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      {bookLoading && (
+        <div className="text-green-500 font-bold bg-green-100 p-3 mt-3 rounded">
+          Loading book data...
+        </div>
       )}
-    </Formik>
+      {patronLoading && (
+        <div className="text-green-500 font-bold bg-green-100 p-3 mt-3 rounded">
+          Loading patron data...
+        </div>
+      )}
+      {bookError && (
+        <div className="text-red-500 font-bold bg-red-100 p-3 mt-3 rounded">
+          {bookError}
+        </div>
+      )}
+      {patronError && (
+        <div className="text-red-500 font-bold bg-red-100 p-3 mt-3 rounded">
+          {patronError}
+        </div>
+      )}
+    </div>
   );
 };
 
